db64b1d25c352c757a7ba97613b02483d4c91074
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-08-09 14:08:12 +0200

    Add project structure exercises

--- react-course-v3-main/03-advanced-react/src/App.jsx
+++ react-course-v3-main/03-advanced-react/src/App.jsx
@@ -1,11 +1,7 @@
-import Starter from './tutorial/02-useEffect/starter/05-cleanup-function';
-import Final from './tutorial/02-useEffect/final/05-cleanup-function';
-
 function App() {
   return (
     <div className="container">
-      <Starter />
-      <Final />
+      <Example />
     </div>
   );
 }
---
+++ react-course-v3-main/03-advanced-react/src/tutorial/04-project-structure/starter/Example/index.jsx
@@ -0,0 +1,13 @@
+import FirstComponent from './FirstComponent';
+import SecondComponent from './SecondComponent';
+
+const Example = () => {
+  return (
+    <section>
+      <div>Example</div>
+      <FirstComponent />
+      <SecondComponent />
+    </section>
+  );
+};
+export default Example;
