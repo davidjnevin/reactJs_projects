e4abc1944692b15c511cdda8ab9ce6f81c5384ac
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-08-15 17:21:23 +0200

    Add lorum ipsum app

--- react-course-v3-main/04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
+++ react-course-v3-main/04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
@@ -1,4 +1,43 @@
+import { useState } from "react";
+import data from "./data";
+import { nanoid } from 'nanoid';
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+	const [count, setCount] = useState(1);
+	const [text, setText] = useState([]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		let amount = parseInt(count);
+		if (count <= 0) {
+			amount = 1;
+		}
+		if (count > 8) {
+			amount = 8;
+		}
+		setText(data.slice(0, amount));
+	};
+
+	return (
+		<section className="section-center">
+			<h3>tired of boring lorem ipsum?</h3>
+			<form className="lorem-form" onSubmit={handleSubmit}>
+				<label htmlFor="amount">paragraphs:</label>
+				<input
+					type="number"
+					name="amount"
+					id="amount"
+					value={count}
+					onChange={(e) => setCount(e.target.value)}
+				/>
+				<button type="submit" className="btn">generate</button>
+			</form>
+			<article className="lorem-text">
+				{text.map((item) => {
+					return <p key={nanoid}>{item}</p>;
+				})}
+			</article>
+		</section>
+	)
 };
 export default App;
