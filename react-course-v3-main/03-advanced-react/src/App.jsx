57e87ecb1c46931894c5c526415013ff9bb778b0
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-08-15 17:20:39 +0200

    Add forms tutorials

--- react-course-v3-main/03-advanced-react/src/App.jsx
+++ react-course-v3-main/03-advanced-react/src/App.jsx
@@ -1,9 +1,12 @@
+import Starter from './tutorial/06-forms/starter/05-form-data';
+import Final from './tutorial/06-forms/final/05-form-data';
+
 function App() {
-  return (
-    <div className="container">
-      <Example />
-    </div>
-  );
+	return (
+		<div className="container">
+			<Starter />
+		</div>
+	);
 }
 
 export default App;
