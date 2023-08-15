a5a751d66ce228a8ea99ae336028a2b5801912fe
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-08-15 19:44:37 +0200

    Add color generator  app

--- react-course-v3-main/04-fundamental-projects/09-color-generator/starter/src/App.jsx
+++ react-course-v3-main/04-fundamental-projects/09-color-generator/starter/src/App.jsx
@@ -1,4 +1,27 @@
+import { useState } from "react";
+import Form from "./components/Form";
+import ColorList from "./components/ColorList";
+import Values from "values.js";
+import { ToastContainer, toast } from "react-toastify";
+
 const App = () => {
-  return <h2>Color Generator Starter</h2>;
+	const [colors, setColors] = useState(new Values('#f15025').all(10));
+
+	const addColor = (color) => {
+		try {
+			const colors = new Values(color).all(10);
+			setColors(colors);
+		} catch (error) {
+			toast.error(error.message);
+		};
+	};
+
+	return (
+		<section>
+			<Form addColor={addColor} />
+			<ColorList colors={colors} />
+			<ToastContainer position="top-center" />
+		</section>
+	);
 };
 export default App;
